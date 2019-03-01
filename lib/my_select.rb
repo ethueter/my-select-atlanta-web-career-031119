1a5a96a58ad7dc93c84f2643f280191add87e454
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-01 00:16:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ block_given?
+ x = 0 
+ new_array = []
+ while x > collection.length 
+ new_array.push(yield(collection))
+ new_array.select {|num| num.even?}
+ 
+ end 
+ new_array
 end
